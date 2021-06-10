bea94438727ed62a17494ada6739c42a68617a0a
ravan2202 <iamvikivivek2202@gmail.com>
2021-06-10 20:02:17 +0530

    1st_edition

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+import logo from "./popcorn.png"
+import logo1 from "./movie.png"
+const Header = () => {
+    return <div className="header"><img id="logo" src={logo1} alt="logo"/>
+    Sadda Adda  <img id="logo" src={logo} alt="logo"/> </div>
+}
+
+export default Header
