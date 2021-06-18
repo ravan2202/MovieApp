8cdad0343b58a691d9296f629aa4c9f83b567ad8
ravan2202 <iamvikivivek2202@gmail.com>
2021-06-18 12:04:48 +0530

    final

--- src/Components/SingleContent/SingleContent.js
+++ src/Components/SingleContent/SingleContent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Badge} from "@material-ui/core"
 import { img_300, unavailable } from '../../config/config'
 import "./SingleContent.css"
-
+import ContentModel from "../ContentModal/ContentModal"
 const SingleContent = ({
     id,
     poster,
@@ -11,14 +11,14 @@ const SingleContent = ({
     vote_average,
 }) => {
     return (
-        <div className='media'>
+        <ContentModel media_type={media_type} id={id}>
             <Badge badgeContent={vote_average}
              color={vote_average > 6 ? "primary" : "secondary"} />
             <img className='poster' src={poster ? `${img_300}/${poster}`: unavailable} alt={title}/>
             <b className='title'>{title}</b>
             <span className='subTitle'>{media_type==='tv'?'Tv Series':'Movies'}
             </span>
-        </div>
+        </ContentModel>
     )
 }
 
